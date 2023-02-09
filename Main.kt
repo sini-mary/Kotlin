c4e6810d0ee2ef4ef4a6d57edae7bfad27fced3d
sini-mary <marysnaidaa@gmail.com>
2023-02-09 07:02:25 +0300

    variables

---
+++ Main.kt
@@ -0,0 +1,35 @@
+fun main(){
+    greeting("Snaida")
+
+}
+
+fun greeting(name : String){
+    println("Hello" + name)
+
+
+}
+fun division(num1: Int , num2: Int) :Int {
+return(num1 % num2)
+}
+var num1= 120
+var num2= 11
+var result = num1 % num2s
+
+
+
+
+fun add(num1: Int, num2 :Int , num3 : Int , num4 : Int): Int {
+    var add= (num1 + num2 + num3 + num4)
+    return add
+
+
+}
+fun fact() {
+    var fact = "I am an athlete"
+    println("I am an athlete")
+
+}
+
+
+
+
